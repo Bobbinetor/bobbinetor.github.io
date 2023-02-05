530ddb7cc1c0e93ff9348f67a17e7976ee94f7f3
Bobbinetor <petruolo95@gmail.com>
2023-02-05 18:40:28 +0100

    yea

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-const treasure = document.getElementById("treasure");
-const result = document.getElementById("result");
-
-
-
-treasure.addEventListener("click", function() {
-  result.textContent = "Congratulations!";
-  result.classList.add("bold");
-});
-
-gameBoard.addEventListener("click", function(event) {
-  const distance = calculateDistance(event, treasure);
-  if (distance < 20) {
-    treasure.style.backgroundColor = "green";
-    result.textContent = "Congratulations!";
-    result.classList.add("bold");
-  } else {
-    result.textContent = "Try Again";
-    result.classList.add("bold");
+window.addEventListener("load", () => {
+  for (let i of document.querySelectorAll(".gallery img")) {
+    i.onclick = () => i.classList.toggle("full");
   }
 });
